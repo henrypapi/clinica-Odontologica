6d34906bc61f41f4d72cfcad173620ebf7b6eb85
Henry_VS <henryvs2003@hotmail..com>
2026-04-28 11:34:23 -0500

    implementacion de rutas para despliegue terminado

--- frontend/ClinicaOdontologica/src/app/shared/components/odontograma-ver/odontograma.service.ts
+++ frontend/ClinicaOdontologica/src/app/shared/components/odontograma-ver/odontograma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import { Observable } from 'rxjs';
 export class OdontogramaService {
   
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/api/odontogramas';
+  private apiUrl = `${environment.apiUrl}/api/odontogramas`;
 
   guardarOdontograma(datosOdontograma: any): Observable<any> {
     
--- frontend/ClinicaOdontologica/src/environments/environment.ts
+++ frontend/ClinicaOdontologica/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://clinica-odontologica-hazel.vercel.app/api' 
+  apiUrl: 'https://clinica-odontologica-1-5uyo.onrender.com' 
 };
